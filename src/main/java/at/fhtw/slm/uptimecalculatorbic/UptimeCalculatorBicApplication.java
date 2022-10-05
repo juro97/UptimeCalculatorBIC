1d433c4a548dc170d4f62f8520ab839cfa0c256e
pjuro97 <p.jurassovits@gmail.com>
2022-10-05 19:24:28 +0200

    initial commit

---
+++ src/main/java/at/fhtw/slm/uptimecalculatorbic/UptimeCalculatorBicApplication.java
@@ -0,0 +1,14 @@
+package at.fhtw.slm.uptimecalculatorbic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UptimeCalculatorBicApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(UptimeCalculatorBicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/fhtw/slm/uptimecalculatorbic/UptimeCalculatorBicApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.slm.uptimecalculatorbic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UptimeCalculatorBicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
